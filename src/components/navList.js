8cdc3b2d21b7bf8ff3bfeebecea21c6625475e63
Karuma Lin <qscwdvefb3@gmail.com>
2023-01-08 00:03:19 +0800

    #

--- src/components/navList.js
+++ src/components/navList.js
@@ -3,10 +3,10 @@ const navList = (type) => {
   if(type == "guest"){
     str = `
       <li class="nav-item mx-auto mb-2 mb-lg-0 me-lg-4">
-        <a class="text-decoration-none link-secondary" href="./#login.html">登入</a>
+        <a class="text-decoration-none link-secondary" href="./login.html">登入</a>
       </li>
       <li class="nav-item mx-auto mb-2 mb-lg-0">
-        <a class="btn btn-primary" href="./#signUp.html">免費註冊</a>
+        <a class="btn btn-primary" href="./signUp.html">免費註冊</a>
       </li>
     `;
   }else if(type == "user"){
@@ -16,7 +16,7 @@ const navList = (type) => {
         <p class="mb-0">Hello！${name}</p>
       </li>
       <li class="nav-item mx-auto mb-2 mb-lg-0 me-lg-4">
-        <a class="text-decoration-none link-secondary" href="./#favoriteList.html">收藏列表</a>
+        <a class="text-decoration-none link-secondary" href="./favoriteList.html">收藏列表</a>
       </li>
       <li class="nav-item mx-auto mb-2 mb-lg-0">
         <a id="logout" class="text-decoration-none link-secondary" href="#">登出</a>
@@ -29,10 +29,10 @@ const navList = (type) => {
         <p class="mb-0">Hello！${name}</p>
       </li>
       <li class="nav-item mx-auto mb-2 mb-lg-0 me-lg-4">
-        <a class="text-decoration-none link-secondary" href="./#backstage.html">前往後台</a>
+        <a class="text-decoration-none link-secondary" href="./backstage.html">前往後台</a>
       </li>
       <li class="nav-item mx-auto mb-2 mb-lg-0 me-lg-4">
-        <a class="text-decoration-none link-secondary" href="./#favoriteList.html">收藏列表</a>
+        <a class="text-decoration-none link-secondary" href="./favoriteList.html">收藏列表</a>
       </li>
       <li class="nav-item mx-auto mb-2 mb-lg-0">
         <a id="logout" class="text-decoration-none link-secondary" href="#">登出</a>
